10e367e6e611939c60ad82ad2062f9149b6e1cd9
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-04-27 11:40:36 -0230

    day vs night shift buttons

--- edit.js
+++ edit.js
@@ -11,71 +11,86 @@ displayFullDate();
 
 
 // adding Day vs Night shift under the date
-function currentShift ()
-{
-    const dayNight = document.getElementById("current-shift");
-}
+// Get the day and night buttons and the shift text element
+const dayButton = document.querySelector('.day-button');
+const nightButton = document.querySelector('.night-button');
+const shiftText = document.querySelector('#shift-text');
 
-// Factory pattern
-// const createNurse = (name, tasks, patients) =>
-const nurse = (name, break_time, code, duties) =>
-{
-  const nurse_info = {
-    name,
-    break_time,
-    code,
-    duties,
-  }
-
-  return {
-    nurse_info,
-    review_info: () => {
-      console.log(nurse_info)
-    },
-    register: () => {
-      localStorage.setItem(nurse_info.name, JSON.stringify(nurse_info))
-    }
-  }
-}
-
-// // called the factorty pattern to initialize a new nurse object
-// const hiba_mohammed = createNurse(
-//   'Hiba Mohammed',
-//   {
-//     'check_medecine': 'Check stock on all medications in RM 413',
-//     'create_schedule': 'Create schedule for the next shift',
-//     'update_fire_code': 'Ensure all relevant fire codes are up to date for the current shift'
-//   },
-//   {
-//     'Clark Oake': {room: 'RM413', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
-//     'Jan Mertlik': {room: 'RM415', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
-//     'Sahand Seifi': {room: 'RM413', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
+// Add a click event listener to the day button
+dayButton.addEventListener('click', function() {
+  shiftText.textContent = "Day Shift";
+  
+  // Hide the day and night buttons
+  dayButton.style.display = "none";
+  nightButton.style.display = "none";
+});
+nightButton.addEventListener('click', function() {
+  shiftText.textContent = "Night Shift";
+  
+  // Hide the day and night buttons
+  dayButton.style.display = "none";
+  nightButton.style.display = "none";
+});
+// // Factory pattern
+// // const createNurse = (name, tasks, patients) =>
+// const nurse = (name, break_time, code, duties) =>
+// {
+//   const nurse_info = {
+//     name,
+//     break_time,
+//     code,
+//     duties,
 //   }
-// );
 
-// used the created nurse object to call factory methods
-// hiba_mohammed.review_info();
-// hiba_mohammed.register();
+//   return {
+//     nurse_info,
+//     review_info: () => {
+//       console.log(nurse_info)
+//     },
+//     register: () => {
+//       localStorage.setItem(nurse_info.name, JSON.stringify(nurse_info))
+//     }
+//   }
+// }
 
-// retrieved stored JSON data from localStorage
-const retrieveNurseFromStorage = (nurse_name) => {
+// // // called the factorty pattern to initialize a new nurse object
+// // const hiba_mohammed = createNurse(
+// //   'Hiba Mohammed',
+// //   {
+// //     'check_medecine': 'Check stock on all medications in RM 413',
+// //     'create_schedule': 'Create schedule for the next shift',
+// //     'update_fire_code': 'Ensure all relevant fire codes are up to date for the current shift'
+// //   },
+// //   {
+// //     'Clark Oake': {room: 'RM413', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
+// //     'Jan Mertlik': {room: 'RM415', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
+// //     'Sahand Seifi': {room: 'RM413', condition: 'stable', vitals: {heart_rate: 90, weight: 180}},
+// //   }
+// // );
 
-  const nurse_info = JSON.parse(localStorage.getItem(nurse_name))
+// // used the created nurse object to call factory methods
+// // hiba_mohammed.review_info();
+// // hiba_mohammed.register();
 
-  console.log(nurse_info)
+// // retrieved stored JSON data from localStorage
+// const retrieveNurseFromStorage = (nurse_name) => {
 
-  return nurse_info
-}
+//   const nurse_info = JSON.parse(localStorage.getItem(nurse_name))
 
-// Handle the submission of a new nurse creation form
-const handleNurseSubmit = (e) => {
-  e.preventDefault()
+//   console.log(nurse_info)
 
-  const new_nurse = createNurse(nurse_name.value, nurse_break.value, nurse_code.value)
+//   return nurse_info
+// }
 
-  new_nurse.register();
-  console.log(new_nurse.nurse_info);
-}
+// // Handle the submission of a new nurse creation form
+// const handleNurseSubmit = (e) => {
+//   e.preventDefault()
 
-retrieveNurseFromStorage('Clark');
+//   const new_nurse = createNurse(nurse_name.value, nurse_break.value, nurse_code.value)
+
+//   new_nurse.register();
+//   console.log(new_nurse.nurse_info);
+// }
+
+// retrieveNurseFromStorage('Clark');
 
